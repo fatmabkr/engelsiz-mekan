4d55d6f0710a01a54854d504dd35d2907be90f82
fatmabkr <fatmabakir895@gmail.com>
2026-07-30 19:12:31 +0300

    Initialize Firebase Analytics in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { initAnalytics } from './firebase';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+// Başlangıçta (production) Analytics'i başlat ve temel page_view gönder
+if (import.meta.env.PROD) {
+  initAnalytics().then((analytics) => {
+    if (analytics) {
+      import('firebase/analytics').then(({ logEvent }) => {
+        try {
+          logEvent(analytics, 'page_view', { page_path: window.location.pathname });
+        } catch (e) {
+          // ignore
+        }
+      });
+      // eslint-disable-next-line no-console
+      console.log('Firebase Analytics başlatıldı');
+    }
+  });
+}
